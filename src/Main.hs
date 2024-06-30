54fc6ec978ae0cf92c6656ef902dc6bc9e40a15f
lucasschiavo <lucasslongoschiavo@gmail.com>
2024-06-30 11:03:31 -0300

    create interpreter file and refactors rename parser monad
    moved logic to interpreter file
    
    remove unnecessary stuff

--- src/Main.hs
+++ src/Main.hs
@@ -2,79 +2,15 @@ module Main (main) where
 
 import AST
 import Data.String (fromString)
-import Debug.Trace (trace)
+import Interpreter
 import Lexer (runAlex)
-import Parser (parseMiniML)
+import Parser (parseProgram)
 import System.IO ()
-
-toLambdaExp :: Prog -> Exp
-toLambdaExp (Prog decs exp) = go (reverse decs) exp
- where
-  go [] exp = exp
-  go ((Dec name def) : xs) exp = App (Lambda name next) def
-   where
-    next = go xs exp
-
-alphaConvert :: Name -> Name -> Exp -> Exp
-alphaConvert name oldName lamb@(Lambda _ exp) =
-  Lambda name $ substitute oldName (Var name) exp
-
-isFreeVar :: Name -> Exp -> Bool
-isFreeVar name (Var varName) = name == varName
-isFreeVar name (Lambda varName exp) = name /= varName && isFreeVar name exp
-isFreeVar name (App exp1 exp2) = isFreeVar name exp1 || isFreeVar name exp2
-
-generateNewName :: Name -> Exp -> Name
-generateNewName name exp
-  | isFreeVar name exp = generateNewName newName exp
-  | otherwise = name
- where
-  newName = name ++ "'"
-
--- Lambda "x" (Lambda "y" (App (Var "x") (App (Var "y") (Var "z"))))
-substitute :: Name -> Exp -> Exp -> Exp
-substitute name def var@(Var varName)
-  | name == varName = def
-  | otherwise = var
-substitute name def (App exp1 exp2) =
-  App (substitute name def exp1) (substitute name def exp2)
-substitute name def lamb@(Lambda varName exp)
-  | name == varName = lamb
-  | not $ isFreeVar varName def = Lambda varName $ substitute name def exp
-  | otherwise = Lambda newVar (substitute name def (substitute varName (Var newVar) exp))
- where
-  newVar = generateNewName varName (App def exp)
-
-redex :: Exp -> Exp -> Exp
-redex lamb@(Lambda name exp) right =
-  substitute name right exp
-redex _ right = right
-
-step :: Exp -> Exp
-step var@(Var{}) = var
-step (Lambda name exp) = Lambda name (step exp)
-step (App lamb@(Lambda{}) right) = redex lamb right
-step (App left right) = App (step left) right
-
-run :: Exp -> Exp
-run exp
-  | exp == next = next
-  | otherwise = run next
- where
-  next = step exp
-
-runPrint :: Exp -> IO ()
-runPrint exp = do
-  let next = step exp
-  print next
-  if next == exp
-    then return ()
-    else runPrint next
 
 getProg :: String -> IO (Maybe Prog)
 getProg path = do
   file <- readFile path
-  case runAlex (fromString file) parseMiniML of
+  case runAlex (fromString file) parseProgram of
     Right ast -> return $ Just ast
     Left err -> do
       print err
--- src/Parser.y
+++ src/Parser.y
@@ -1,7 +1,7 @@
 {
 {-# LANGUAGE DeriveFoldable #-}
 module Parser
-  ( parseMiniML
+  ( parseProgram
   ) where
 
 import Data.ByteString.Lazy.Char8 (ByteString, unpack)
@@ -13,7 +13,7 @@ import qualified Token as T
 import qualified Lexer as L
 }
 
-%name parseMiniML program
+%name parseProgram program
 %tokentype { L.RangedToken }
 %error { parseError }
 %monad { L.Alex } { >>= } { pure }
@@ -65,18 +65,12 @@ parseError _ = do
 lexer :: (L.RangedToken -> L.Alex a) -> L.Alex a
 lexer = (=<< L.alexMonadScan)
 
--- | Build a simple node by extracting its token type and range.
 unTok :: L.RangedToken -> (T.Token -> a) -> a
 unTok (L.RangedToken tok range) ctor = ctor tok
 
--- | Unsafely extracts the the metainformation field of a node.
 info :: Foldable f => f a -> a
 info = fromJust . getFirst . foldMap pure
 
--- | Performs the union of two ranges by creating a new range starting at the
--- start position of the first range, and stopping at the stop position of the
--- second range.
--- Invariant: The LHS range starts before the RHS range.
 (<->) :: L.Range -> L.Range -> L.Range
 L.Range a1 _ <-> L.Range _ b2 = L.Range a1 b2
 }
