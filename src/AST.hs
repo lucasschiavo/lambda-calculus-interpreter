e9b788733cc95ba6035e0f34540ad5bfa03a31fc
lucasschiavo <lucasslongoschiavo@gmail.com>
2024-07-06 14:06:54 -0300

    change show instance of Expr
    makes printing a expression a little better for the eyes

--- src/AST.hs
+++ src/AST.hs
@@ -10,8 +10,16 @@ data Exp
   = Var Name
   | Lambda Name Exp
   | App Exp Exp
-  deriving (Show, Eq)
+  deriving (Eq)
 
 data Prog
   = Prog [Dec] Exp
   deriving (Show)
+
+instance Show Exp where
+  show (Var name) = name
+  show (Lambda name exp) = "\\" ++ name ++ "." ++ show exp
+  show (App (Var name1) (Var name2)) = name1 ++ " " ++ name2
+  show (App (Var name) exp2) = name ++ " (" ++ show exp2 ++ ")"
+  show (App exp1 (Var name)) = "(" ++ show exp1 ++ ") " ++ name
+  show (App exp1 exp2) = "(" ++ show exp1 ++ ") " ++ "(" ++ show exp2 ++ ")"
